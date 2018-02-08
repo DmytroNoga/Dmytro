ecb7939c3a6c40f9d3bf9664a39899cb7e68956c
DmytroNoga <36132893+DmytroNoga@users.noreply.github.com>
2018-02-08 11:01:55 +0200

    Update __toString.php

--- __toString.php
+++ __toString.php
@@ -1,24 +1,11 @@
 <?php
-/*class Test{
-    public $name;
-    public function __construct($name) {
-       $this->name = $name;
-    }
-    public function __toString() {
-        return $this->name;
-    }
-}
-$class = new Test(1);
-var_dump($class);
-die();*/
-
 class Machine {
 	public $name;
 	public $model;
 	public $price;
 	public $maxpassagers;
 
-	public function __construct($name, $model, $price, $maxpassagers) {
+	public function __construct($name, $model, $price,$maxpassagers) {
 		$this->name = $name;
 		$this->model = $model;
 		$this->price = $price;
@@ -26,26 +13,37 @@ class Machine {
 	}
 
 	public function __toString() {
-		return $this->name;
-		return $this->model;
-		return $this->price;
-		return $this->maxpassagers;
+		return Mashine;
 	}
+
 }
 class Cars extends Machine {
-
+	public function __toString() {
+		return Cars;
+	}
 }
 
 class Bus extends Machine {
-	
+	public function __toString() {
+		return Bus;
+	}	
 }
 
 class Moto extends Machine {
-
+	public function __toString() {
+		return Moto;
+	}
 }
 
 $product1 = new Cars('WV', 'B5', 12, 4);
+$product2 = new Bus('Etalon', '100500', 3, 22);
+$product3 = new Moto('Honda', 'Giorno', 5, 1);
+echo '<br>'.$product1.'<br>';
 var_dump($product1);
+echo '<br>'.$product2.'<br>';
+var_dump($product2);
+echo '<br>'.$product3.'<br>';
+var_dump($product3);
 echo '<br>';
 die();
 ?>
